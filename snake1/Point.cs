4fdff3703fb8f75cefbc632b265274d486601467
Relaxens <kaled1992@yandex.ru>
2018-02-04 12:54:07 +0700

    point

--- snake1/Point.cs
+++ snake1/Point.cs
@@ -11,5 +11,10 @@ namespace snake1
         public int x;
         public int y;
         public char sym;
+        public void Drow()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
