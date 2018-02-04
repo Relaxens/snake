08abb35b5c95a7e16ef6f7320b4fcda6f4f215f8
Relaxens <kaled1992@yandex.ru>
2018-02-04 14:44:14 +0700

    Lines

--- snake1/Program.cs
+++ snake1/Program.cs
@@ -18,37 +18,12 @@ namespace snake1
 
             p2.Drow();
 
-            Point p3 = new Point(7, 10, '&');
-            Point p4 = new Point(8, 11, '(');
-            Point p5 = new Point(9, 12, ')');
-            Point p6 = new Point(10, 13, '%');
+            HorizontalLine line = new HorizontalLine(5,10,8,'*');
+            line.Drow();
 
-            List<Point> newpList = new List<Point>();
-            newpList.Add(p3);
-            newpList.Add(p4);
-            newpList.Add(p5);
-            newpList.Add(p6);
+            VerticalLine vline = new VerticalLine(5,10,15,'+');
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
+            vline.Drow();
 
 
             Console.ReadLine();
