eadfe1bbf065f0f6c42729d7ab1373168415fc57
Relaxens <kaled1992@yandex.ru>
2018-02-04 12:26:46 +0700

    Drow

--- snake1/Program.cs
+++ snake1/Program.cs
@@ -14,18 +14,20 @@ namespace snake1
             int y1 = 3;
             char sym1 = '*';
 
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
+            Drow(x1, y1, sym1);
 
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
 
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
-
+            Drow(x2, y2, sym2);
 
             Console.ReadLine();
+        }
+        static void Drow(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
