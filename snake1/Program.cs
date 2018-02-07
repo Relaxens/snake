669583e838b3c5944b57d0a9b729aaab2b7a559c
Relaxens <kaled1992@yandex.ru>
2018-02-07 22:07:48 +0700

    1

--- snake1/Program.cs
+++ snake1/Program.cs
@@ -11,47 +11,13 @@ namespace snake1
     {
         static void Main(string[] args)
         {
-            /*  VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-             Draw(v1);
-
-             Point p = new Point(4, 5, '*');
-             Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-             Draw(fSnake);
-             Snake snake = (Snake)fSnake;
-
-             HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-
-             List<Figure> figures = new List<Figure>();
-             figures.Add(fSnake);
-             figures.Add(v1);
-             figures.Add(h1);
-
-             foreach (var f in figures)
-             {
-                 f.Draw();
-             }
-         }
-          static void Draw(Figure figure)
-             {
-                 figure.Draw();
-             }
-             */
 
             Console.SetBufferSize(80, 25);
 
             Walls walls = new Walls(80, 25);
             walls.Draw();
 
-            /*
-               HorizontalLine upLine = new HorizontalLine(0,78,0,'+');
-               HorizontalLine downLine = new HorizontalLine(0, 78,24, '+');
-               VerticalLine leftLine = new VerticalLine(0, 24, 78, '+');
-               VerticalLine rightLine = new VerticalLine(0,24,0,'+');
-               downLine.Draw();
-               upLine.Draw();
-               leftLine.Draw();
-               rightLine.Draw(); 
-               */
+
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
